e6fc64df1042e5c37b25d484342c57fc3c743791
Shaurya Vardhan Singh <gsshaurya@gmail.com>
2022-09-25 23:23:33 +0530

    ipfs fix

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import express from "express";
 var cors = require("cors");
 import multer from "multer";
 import bodyParser from "body-parser";
-import { saveToIPFS } from "./ipfs.js";
+import { saveToIPFS, saveAsJSONToIPFS } from "./ipfs.js";
+import fs from "fs";
+import { getFilesFromPath } from "web3.storage";
 
 const upload = multer();
 
@@ -12,7 +14,7 @@ const router = express.Router();
 const BASE_URL = "/web3_storage";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,18 +25,40 @@ router.post(
   cors(),
   async (req, res) => {
     const { name, description } = req.body;
-    console.log(req.files);
+    console.log(req.files, name, description);
     const results = await Promise.all(
       req.files.map(async (item, index) => {
         try {
           let mediaUrl;
-          console.log(Buffer.from(item.buffer).toString("base64"));
-          mediaUrl = await saveToIPFS(
-            `data:image/gif;base64,${Buffer.from(item.buffer).toString(
-              "base64"
-            )}`
+          console.log(
+            "base 64 string is",
+            Buffer.from(item.buffer).toString("base64")?.slice(0, 100)
           );
-          console.log(`https://${mediaUrl}.ipfs.w3s.link`);
+          const url = Buffer.from(item.buffer).toString("base64");
+          let buff = Buffer(url, "base64");
+          fs.writeFileSync("graph.png", buff);
+
+          const files = await getFilesFromPath("./graph.png");
+
+          // const res = await fetch(`data:image/png;base64,${url}`);
+          // const fileBlob = await res.blob();
+
+          // const file = new File([fileBlob], "File name", { type: "image/png" });
+          mediaUrl = await saveToIPFS(files);
+          console.log(`https://${mediaUrl}.ipfs.w3s.link/graph.png`);
+          const nftMetadata = {
+            image: `https://${mediaUrl}.ipfs.w3s.link/graph.png`,
+            name,
+            description,
+          };
+          const metadataCid = await saveAsJSONToIPFS(nftMetadata);
+          console.log("metadataCid", metadataCid);
+          res
+            .status(200)
+            .send({
+              status: true,
+              metadata_uri: `https://${metadataCid?.cid}.ipfs.w3s.link/file.json`,
+            });
         } catch (error) {
           console.log("error", error);
           throw error;
--- ipfs.js
+++ ipfs.js
@@ -5,8 +5,7 @@ import { Web3Storage, File, Blob } from "web3.storage";
 // const { Web3Storage, File } = require("web3.storage");
 
 const API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDA5NDg2MjA3NDAxMjJDOTFkZWNlNzUwRDFEMDhFM0ZFOEUwMDQ1MDEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjEyODExMjU4MTEsIm5hbWUiOiJ3ZWIzLXRlc3QifQ.Ja5fln0-MAU2H4dmNuq2i3RQ-VXzKpl9RH-By2bcvkw";
-
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGM1N0NlZDJGOTEwQzBDYzk4NEY2N0U5YkJCOTA1OTYxREFFODAzZUQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjQxMTk4NjExMzQsIm5hbWUiOiJldGgtb25saW5lIn0.EA1pP5CsJ6FrjYapuSmAHYmIJc8IqXAyxr0eUIPeQqo";
 const client = new Web3Storage({ token: API_KEY });
 const data = {
   name: "token name",
